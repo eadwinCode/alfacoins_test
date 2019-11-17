a5d602b46512970c11119c849305efbbefd093c1
Ezeudoh Tochukwu <dlastdon4000@gmail.com>
2019-11-17 06:15:01 +0000

    working on notification data

--- alfacoins_model/urls.py
+++ alfacoins_model/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import payment_notification, PaymentList, PaymentSetupView
+from .views import payment_notification, PaymentList, PaymentSetupView, PaymentDetail
 
 urlpatterns = [
     path('payments', PaymentList.as_view(), name="payment_list"),
     path('', PaymentSetupView.as_view(), name="payment_setup"),
+    path('payments/<uuid:pk>', PaymentDetail.as_view(), name='payment_detail'),
     path('notification/payments', payment_notification, name='ipn-payment')
 ]
